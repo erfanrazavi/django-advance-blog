ddb146bea925dd20f37badcff8da429f393aa240
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2024-12-13 18:26:13 +0330

    create admin file in account app
    create custom user class for admin(DB)

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email' , 'is_superuser' , 'is_active',)
+    list_filter = ("email", "is_superuser", "is_active",)
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ('Authentication' , {
+            "fields":(
+                "email" , "password"
+            ),
+        }),
+        ('Permissions' , {
+            "fields":("is_staff" ,'is_active' ,"is_superuser"),
+        }),
+    )
+    add_fieldsets = (
+        ('Add User', {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1","password2" , "is_active", "is_staff",
+                "is_superuser"
+            )}
+        ),
+    )
+
+admin.site.register(User, CustomUserAdmin)
