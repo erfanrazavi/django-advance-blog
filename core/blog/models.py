16b9a62302a6b6ddbcd6d88431552501b8aeb02a
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2024-12-14 03:07:31 +0330

    add static and media setting in setting file

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
+#getting user model object
+User = get_user_model()
 
 class Post(models.Model):
     '''
