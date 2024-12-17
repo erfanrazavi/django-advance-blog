1ac445174fb8668133bc46a37c5c9c81fb99a290
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2024-12-17 12:56:41 +0330

    django list view
    craete url and view for django list view

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -6,5 +6,6 @@ app_name = "blog"
 
 urlpatterns = [
     path("cbv", IndexView.as_view(),name='cbv-i'),
+    path("list-view/", PostList.as_view(),name='list-cbv'),        
     path("go-to-google", RedirectToGoogle.as_view(), name="redirect-google"),
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render , HttpResponse
 from django.views.generic.base import TemplateView , RedirectView
+from django.views.generic import ListView 
 from .models import Post
 from django.shortcuts import get_object_or_404
 # Create your views here.
@@ -15,6 +16,22 @@ class IndexView(TemplateView):
         context["name"] = 'erf'
         context['posts'] = Post.objects.all()
         return context
+    
+
+class PostList(ListView):
+    '''
+    a class based view (ListView) to show list of data into db
+    
+    '''
+    # def get_queryset(self):
+    #     posts = Post.objects.filter(status = True)
+    #     return posts
+    # queryset = Post.objects.filter(status = True)
+
+    model = Post
+    context_object_name = 'posts'
+    paginate_by =  1
+    ordering = '-id'
     
 class RedirectToGoogle(RedirectView):
     url = 'https://google.com'
