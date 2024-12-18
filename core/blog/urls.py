18c49383704754d214f19f837678b11c6c54a331
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2024-12-18 18:27:37 +0330

    add following titles
    -add permissionrequierdmixin
    -add loginrequierdmixin\-add deleteview
    -add updateview
    -add create view
    -add formview
    add detail view
    -add login and log out html file
    -add accounts in core url

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -5,7 +5,13 @@ from django.views.generic import TemplateView , RedirectView
 app_name = "blog"
 
 urlpatterns = [
-    path("cbv", IndexView.as_view(),name='cbv-i'),
-    path("list-view/", PostList.as_view(),name='list-cbv'),        
-    path("go-to-google", RedirectToGoogle.as_view(), name="redirect-google"),
+    # path("cbv", IndexView.as_view(),name='cbv-i'),
+    path("post/", PostList.as_view(),name='list-cbv'),        
+    # path("go-to-google", RedirectToGoogle.as_view(), name="redirect-google"),
+    path('post/<int:pk>/' , PostDetailView.as_view() , name = 'post-detail'),
+    path('post/create/' , PostCreateView.as_view() , name = 'post-create'),
+    path('post/<int:pk>/edit/' , PostUpdateView.as_view() , name = 'post-edit'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,40 +1,76 @@
 from django.shortcuts import render , HttpResponse
 from django.views.generic.base import TemplateView , RedirectView
-from django.views.generic import ListView 
+from django.views.generic import ListView , DetailView ,  FormView , CreateView , UpdateView , DeleteView
+from .forms import PostForm
 from .models import Post
 from django.shortcuts import get_object_or_404
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create your views here.
 
-class IndexView(TemplateView):
-    '''
-        a class based view to show index page
-    '''
+# class IndexView(TemplateView):
+#     '''
+#         a class based view to show index page
+#     '''
      
-    template_name = 'index.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["name"] = 'erf'
-        context['posts'] = Post.objects.all()
-        return context
+#     template_name = 'index.html'
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["name"] = 'erf'
+#         context['posts'] = Post.objects.all()
+#         return context
     
 
-class PostList(ListView):
-    '''
-    a class based view (ListView) to show list of data into db
-    
-    '''
-    # def get_queryset(self):
-    #     posts = Post.objects.filter(status = True)
-    #     return posts
-    # queryset = Post.objects.filter(status = True)
-
-    model = Post
+class PostList( ListView):
+    queryset = Post.objects.all()
     context_object_name = 'posts'
-    paginate_by =  1
     ordering = '-id'
     
-class RedirectToGoogle(RedirectView):
-    url = 'https://google.com'
+from django.utils import timezone
+
+class PostDetailView(LoginRequiredMixin,DetailView):
+    model = Post
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["now"] = timezone.now()
+        return context
+    
+    # def get_context_data(self, **kwargs):
+    #     posts = super().get_context_data(**kwargs)
+    #     posts['title'] = Post.objects.filter(status = 1)
+    #     return posts
+    
+# class RedirectToGoogle(RedirectView):
+#     url = 'https://google.com'
+
 
     
+'''class PostCreateView(FormView):
+
+    template_name = "contact.html"
+    form_class = PostForm
+    success_url = "/blog/post"
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)'''
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    form_class = PostForm
+    # fields = ( 'title' , 'content' , 'status' , 'category',)
+    success_url = '/blog/post/'
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    model = Post
+    form_class = PostForm
+    success_url = '/blog/post/'
+    
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/blog/post'
+
