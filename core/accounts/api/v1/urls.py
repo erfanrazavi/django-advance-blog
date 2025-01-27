ecb2c3d74d94ce29850d5f232f722b0b7fedc04a
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-01-27 12:43:25 +0330

    Implement registration API with password validation and user creation
    - Added registration serializer with password confirmation and validation.
    - Implemented RegistrationApiView for handling registration requests.
    - Defined endpoint for registration in urls.py.

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path , include
 
+from .views import *
 
 
 
-
-app_name = "account-api-v1"
+app_name = "api-v1"
 
 
 
 urlpatterns = [
+    #registration
+    path("registration/" , RegistrationApiView.as_view() , name="registration"),
+    #change password
+    #reset password
+    #login token
+    #login jwt
     
 ]   
---
+++ core/accounts/api/v1/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics
+from .serializers import RegistrationSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class RegistrationApiView(generics.GenericAPIView):
+    serializer_class = RegistrationSerializer
+
+    def post(self, request , *args, **kwargs):
+        serializer = RegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            data = {
+                'email' : serializer.validated_data['email'],
+                'message' : 'User created successfully'
+            }
+            return Response(data, status = status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
+        
