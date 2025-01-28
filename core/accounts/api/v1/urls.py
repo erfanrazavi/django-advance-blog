99c11cfb8a87a20664aa1715d17f2fb302d3d061
Erfan Razavi <101706905+erfanrazavi@users.noreply.github.com>
2025-01-28 15:28:23 +0330

    Add serializer and view for token-based authentication
    - Added CustomAuthTokenSerializer for validating email and password
    - Created CustomAuthToken view to handle token-based login
    - Updated urls.py to include login and logout endpoints for token authentication
    - Minor fixes and optimizations in RegistrationSerializer

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path , include
-
 from .views import *
+from rest_framework.authtoken.views import ObtainAuthToken
+
+
 
 
 
@@ -11,9 +13,13 @@ app_name = "api-v1"
 urlpatterns = [
     #registration
     path("registration/" , RegistrationApiView.as_view() , name="registration"),
+    
     #change password
     #reset password
     #login token
+    path('token/login/', CustomAuthToken.as_view(), name='token-login'),
+    path('token/logout/', CustomDiscardAuthToken.as_view(), name='token-logout'),
+
     #login jwt
     
 ]   
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer , CustomAuthTokenSerializer
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
 
 class RegistrationApiView(generics.GenericAPIView):
     serializer_class = RegistrationSerializer
@@ -19,3 +21,23 @@ class RegistrationApiView(generics.GenericAPIView):
         else:
             return Response(serializer.errors , status=status.HTTP_400_BAD_REQUEST)
         
+
+class CustomAuthToken(ObtainAuthToken):
+    serializer_class = CustomAuthTokenSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
+    
+class CustomDiscardAuthToken(APIView):
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
